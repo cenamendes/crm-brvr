5306397c5d3b4b1e336b7035eca0add4a48164f4
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-02-16 18:31:40 +0000

    [upd] CustomersInterface

--- app/Interfaces/Tenant/Customers/CustomersInterface.php
+++ app/Interfaces/Tenant/Customers/CustomersInterface.php
@@ -2,15 +2,17 @@
 
 namespace App\Interfaces\Tenant\Customers;
 
-use App\Models\Tenant\Customers;
+use App\Models\User;
 
+use App\Models\Tenant\Files;
+use App\Models\Tenant\Customers;
+use App\Models\Tenant\ContactsCustomers;
 use App\Models\Tenant\CustomerLocations;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 use App\Http\Requests\Tenant\Customers\CustomersFormRequest;
 use App\Http\Requests\Tenant\CustomerContacts\CustomerContactsFormRequest;
 use App\Http\Requests\Tenant\CustomerLocations\CustomerLocationsFormRequest;
-use App\Models\Tenant\ContactsCustomers;
-use Illuminate\Database\Eloquent\Collection;
 
 interface CustomersInterface
 {
@@ -27,5 +29,12 @@ interface CustomersInterface
     public function update(Customers $customer,CustomersFormRequest $request): Customers;
 
     public function destroy(Customers $customer) : Customers;
+
+    public function createLogin(string $customer): User;
+
+    ///**** Ficheiros */
+
+    public function getCustomersOfMember(int $id,$perPage): LengthAwarePaginator;
+
 
 }
