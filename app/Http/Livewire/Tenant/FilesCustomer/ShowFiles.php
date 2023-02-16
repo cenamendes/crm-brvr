f8051a259720e6259dff1771900bf514d1b1aef9
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-02-16 18:26:16 +0000

    [new] ShowFiles

---
+++ app/Http/Livewire/Tenant/FilesCustomer/ShowFiles.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Livewire\Tenant\FilesCustomer;
+
+use Livewire\Component;
+use App\Models\Tenant\Files;
+use Livewire\WithPagination;
+use App\Models\Tenant\Customers;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use App\Interfaces\Tenant\Customers\CustomersInterface;
+
+
+class ShowFiles extends Component
+{
+    use WithPagination;
+
+    protected $listeners = ["FilesOfThisCustomer" => "FilesOfThisCustomer"];
+
+    private object $customers;
+    public int $perPage;
+    public string $files = '';
+    public int $customer = 0;
+
+
+    protected object $customersRepository;
+
+    public function boot(CustomersInterface $interfaceCustomer)
+    {
+        $this->customersRepository = $interfaceCustomer;
+    }
+
+    public function mount(): void
+    {
+        if (isset($this->perPage)) {
+            session()->put('perPage', $this->perPage);
+        } elseif (session('perPage')) {
+            $this->perPage = session('perPage');
+        } else {
+            $this->perPage = 10;
+        }
+
+        $customer = Customers::where('user_id',Auth::user()->id)->first();
+        $this->files = Files::where('customer_id',$customer->id)->get();
+        $this->emit('FilesUpdatedFromMembers', $this->files,$this->customer);
+        $this->customer = $customer->id;
+    }
+
+    public function updatedPerPage(): void
+    {
+        $this->resetPage();
+        session()->put('perPage', $this->perPage);
+    }
+
+    public function updatedSearchString(): void
+    {
+        $this->resetPage();
+    }
+
+    public function paginationView()
+    {
+        return 'tenant.livewire.setup.pagination';
+    }
+
+    public function FilesOfThisCustomer($id)
+    {
+       // $this->customer = $id;
+        $this->files = Files::where('customer_id',$this->customer)->get();
+        $this->emit('FilesUpdatedFromMembers', $this->files,$this->customer);
+    }
+
+    /**
+     * List informations of customer location
+     *
+     * @return View
+     */
+    public function render(): View
+    {
+        
+        //$this->customers = $this->customersRepository->getCustomersOfMember(Auth::user()->id,$this->perPage); 
+           
+        return view('tenant.livewire.filescustomer.show', [
+          //  'customers' => $this->customers,
+            'file' => $this->files,
+            'customer' => $this->customer
+        ]);
+    }
+}
