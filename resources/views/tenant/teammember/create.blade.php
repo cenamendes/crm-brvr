c701970b534b2a0f851816b178f5770eb2924342
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-03 15:28:11 +0100

    [upd] teamMember

--- resources/views/tenant/teammember/create.blade.php
+++ resources/views/tenant/teammember/create.blade.php
@@ -1,15 +1,15 @@
-<x-tenant-layout title="_('New Customer')" :themeAction="$themeAction">
+<x-tenant-layout title="_('New Team Member')" :themeAction="$themeAction">
     <div class="container-fluid">
         <div class="page-titles">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="javascript:void(0)">{{ __('Customers') }}</a></li>
+                <li class="breadcrumb-item"><a href="javascript:void(0)">{{ __('Team Members') }}</a></li>
                 <li class="breadcrumb-item active"><a href="javascript:void(0)">{{ __('Create') }}</a></li>
             </ol>
         </div>
         <div class="default-tab">
             <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#home"><i class="la la-home mr-2"></i> {{__("Customer")}}</a>
+                    <a class="nav-link active" data-toggle="tab" href="#home"><i class="la la-home mr-2"></i> {{__("Team Members")}}</a>
                 </li>
                 <li class="nav-item" style="display:none">
                     <a class="nav-link disabled" data-toggle="tab" href="#profile"><i class="la la-phone mr-2"></i> Contacts</a>
