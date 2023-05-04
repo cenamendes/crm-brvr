48dadf1c1d55b7969da9b66d40658178c83be969
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-04 14:48:37 +0100

    [upd] TasksTimes Model

--- app/Models/Tenant/TasksTimes.php
+++ app/Models/Tenant/TasksTimes.php
@@ -15,7 +15,7 @@ class TasksTimes extends Model
     use HasFactory;
     use ComposhipsEagerLimit;
 
-    protected $fillable = ['service_id','task_id', 'date_begin', 'hour_begin','date_end','hour_end', 'total_hours'];
+    protected $fillable = ['service_id','task_id', 'tech_id', 'date_begin', 'hour_begin','date_end','hour_end', 'total_hours'];
 
    
     public function service()
