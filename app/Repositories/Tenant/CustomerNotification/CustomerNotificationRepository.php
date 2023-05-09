71d287fa6e979e48059f2b2e5c8fb211c80ca170
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-09 15:06:51 +0100

    [upd] CustomerNotificationRepository

--- app/Repositories/Tenant/CustomerNotification/CustomerNotificationRepository.php
+++ app/Repositories/Tenant/CustomerNotification/CustomerNotificationRepository.php
@@ -6,6 +6,7 @@ use App\Models\Tenant\Customers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Tenant\CustomerNotifications;
 use App\Interfaces\Tenant\CustomerNotification\CustomerNotificationInterface;
+use App\Models\Tenant\TeamMember;
 
 class CustomerNotificationRepository implements CustomerNotificationInterface
 {
@@ -13,19 +14,37 @@ class CustomerNotificationRepository implements CustomerNotificationInterface
     public function getNotificationTimes(): Array
     {
         $notificationInfo = [];
-        if(Auth::user()->type_user == 2)
+        // if(Auth::user()->type_user == 2)
+        // {
+        //     $customer = Customers::where('user_id',Auth::user()->id)->first();
+        //     $servicesNotifications = CustomerNotifications::where('treated',1)->where('customer_id',$customer->id)->with('service')->with('customer')->with('customerLocation')->get();
+        // }
+        // else 
+        // {
+        //     $servicesNotifications = CustomerNotifications::where('treated',1)->with('service')->with('customer')->with('customerLocation')->get();   
+        // }
+        if(Auth::user()->type_user == 1)
         {
-            $customer = Customers::where('user_id',Auth::user()->id)->first();
-            $servicesNotifications = CustomerNotifications::where('treated',1)->where('customer_id',$customer->id)->with('service')->with('customer')->with('customerLocation')->get();
+            $teamMember = TeamMember::where('user_id',Auth::user()->id)->first();
+           
+            $servicesNotifications = CustomerNotifications::with('service')->with('customerLocation')
+                ->whereHas('customer', function ($query) use($teamMember){
+                   $query->Where('account_manager',$teamMember->id);
+                  
+                })
+                ->where('treated',1)
+                ->get();
+
         }
-        else 
+        else
         {
-            $servicesNotifications = CustomerNotifications::where('treated',1)->with('service')->with('customer')->with('customerLocation')->get();   
+            $servicesNotifications = CustomerNotifications::where('treated',1)->with('service')->with('customer')->with('customerLocation')->get();
         }
 
         foreach($servicesNotifications as $count => $notification)
         {
-            $notificationInfo[$count] = ["customerServicesId" => $notification->id, "service" => $notification->service->name, "date_final_service" => $notification->end_service_date, "customer" => $notification->customer->short_name, "customer_county" => $notification->customerLocation->description, "notification" => $notification->notification_day, "tratada" => $notification->treated];
+            $tm = TeamMember::where('id',$notification->customer->account_manager)->first();
+            $notificationInfo[$count] = ["customerServicesId" => $notification->id, "service" => $notification->service->name, "date_final_service" => $notification->end_service_date, "customer" => $notification->customer->short_name, "team_member" => $tm->name, "customer_county" => $notification->customerLocation->description, "notification" => $notification->notification_day, "tratada" => $notification->treated];
         }
 
         return $notificationInfo;
