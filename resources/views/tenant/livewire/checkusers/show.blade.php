dfdea223f9fa994c254cfb41c4d41a1aa4607253
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-22 16:51:09 +0100

    [upd] checkusers

---
+++ resources/views/tenant/livewire/checkusers/show.blade.php
@@ -0,0 +1,96 @@
+<div>
+    <li class="nav-item dropdown notification_dropdown pl-2">
+        <a class="nav-link bell bell-link" href="javascript:void(0)" role="button"
+            data-toggle="dropdown">
+            <svg width="28" height="28" viewBox="0 0 28 28" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+               d="M19.75,15.67a6,6,0,1,0-7.51,0A11,11,0,0,0,5,26v1H27V26A11,11,0,0,0,19.75,15.67ZM12,11a4,4,0,1,1,4,4A4,4,0,0,1,12,11ZM7.06,25a9,9,0,0,1,17.89,0Z"
+                    fill="#326c91 " />
+            </svg>
+                {{-- <div id="alerticon" class="pulse-css" style="display:none;"></div> --}}
+        </a>
+        
+        <!-- começa aqui -->
+        <div class="chatbox" wire:ignore.self>
+            <div class="chatbox-close"></div>
+            <div class="custom-tab-1">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toogle="tab" href="#chat">Utilizadores</a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane fade active show" id="chat" role="tabpanel">
+                        <div class="card mb-sm-3 mb-md-0 contacts_card ">
+                            <div class="card-body contacts_body p-0 dz-scroll ps ps--active-y" id="DZ_W_Contacts_Body">
+                                <ul class="contacts">
+                                    
+                                    @foreach ($users as $key => $user )
+    
+                                        <li class="name-first-letter">{{$key}}</li>
+
+                                        @foreach ($user as $us)
+                                            
+                                            <li class="active dz-chat-user">
+                                                <div class="d-flex bd-highlight">
+                                                    <div class="img_cont">
+                                                        @if($us["photo"] == null)
+                                                            <img src="{!! "https://".$_SERVER['SERVER_NAME']."/assets/resources/images/avatar/1.png" !!}" class="rounded-circle user_img" alt>
+                                                        @else
+                                                            <img src="{!! global_tenancy_asset('/app/public/profile/'.$us["photo"].'') !!}" class="rounded-circle user_img" alt>
+                                                        @endif
+                                                        @if($us["status"] == "online")
+                                                            <span class="online_icon"></span>
+                                                        @else
+                                                            <span class="online_icon offline"></span>
+                                                        @endif
+                                                    </div>
+                                                    <div class="user_info">
+                                                        <span>{{$us["name"]}}</span>
+                                                        @if($us["status"] == "online")
+                                                            <p>online</p>
+                                                        @else
+                                                            @if($us["last_seen"] != "")
+                                                                @php
+                                                                    $difference = \Carbon\Carbon::parse($us["last_seen"])->diffForHumans();
+                                                                @endphp
+                                                                <p>offline {{$difference}}</p>
+                                                            @endif
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        @endforeach
+                                    @endforeach
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </li>
+    
+@push('custom-scripts')
+    <script>
+         jQuery(document).ready(function(){
+            
+
+            //jQuery(".navbar-nav").on("click", ".bell-link", function(){
+            jQuery(".bell-link").on('click',function() {
+                Livewire.emit("RefreshUserLog");
+            })
+           // 
+
+          
+         })       
+     
+    </script>
+@endpush
+   
+</div>
+
