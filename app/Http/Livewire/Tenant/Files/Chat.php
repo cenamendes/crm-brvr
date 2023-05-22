e67e67a034441ab20dbc8944c95b8b893d116426
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-22 16:34:06 +0100

    [upd] Chat

--- app/Http/Livewire/Tenant/Files/Chat.php
+++ app/Http/Livewire/Tenant/Files/Chat.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Livewire\Tenant\Files;
 
+
+use Carbon\Carbon;
 use App\Models\User;
+
 use Livewire\Component;
 use App\Events\ChatMessage;
 use App\Events\LoginStatus;
 use App\Models\Tenant\Files;
 use Livewire\WithPagination;
 use Livewire\WithFileUploads;
+use Shetabit\Visitor\Visitor;
 use App\Events\ChatMessageAdmin;
+use App\Models\Tenant\Customers;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use App\Events\ChatEmail\ChatEmailEvent;
 use App\Events\ChatMessage as EventsChatMessage;
 use App\Interfaces\Tenant\ChatMessage\ChatInterface;
 use App\Interfaces\Tenant\Customers\CustomersInterface;
@@ -75,6 +82,32 @@ class Chat extends Component
             $this->chat = $this->chatRepository->getMessages($this->customer_id);
             $this->alertRepository->SendNotification(Auth::user()->id,$this->customer_id,"message");
             event(new ChatMessage());
+
+            //verificação se estiver logado ou nao
+            if(Auth::user()->type_user != 2)
+            {
+                $userOfCustomer = Customers::where('id',$this->customer_id)->first();
+
+                $userResult = User::where('id',$userOfCustomer->user_id)->first();
+
+                $startDate = Carbon::parse($userResult->last_seen);
+                $endDate = Carbon::parse(date('Y-m-d H:i:s'));
+
+                $diff = $startDate->diff($endDate);
+
+                $number_minutes = $diff->days * 24 * 60;
+                $number_minutes += $diff->h * 60;
+                $number_minutes += $diff->i;
+
+               if($startDate == $endDate || $number_minutes <= 30){
+               }
+               else {
+                 event(new ChatEmailEvent($this->customer_id));
+               }
+
+                
+            }
+           
         }
     }
 
