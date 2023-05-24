2bfe47d5a8e0162725076200b21786643669ddef
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-24 18:18:19 +0100

    [upd] SendTasks

--- app/Listeners/Tasks/SendDispatchTasksNotification.php
+++ app/Listeners/Tasks/SendDispatchTasksNotification.php
@@ -35,6 +35,43 @@ class SendDispatchTasksNotification
      */
     public function handle(DispatchTasksToUser $task)
     {
+       
+        $json = [
+            "reference" => $task->task->reference, 
+            "customer_name" => $task->task->taskCustomer->name, 
+            "customer_nif" => $task->task->taskCustomer->vat,
+            "descricao" => $task->task->additional_description,
+            "data_prevista" => $task->task->scheduled_date,
+            "hora_prevista" => $task->task->scheduled_hour,
+            "data_criacao" => $task->task->created_at,
+            "nome_tecnico" => $task->task->tech->name,
+            "email_tecnico" => $task->task->tech->email,
+            "origem_pedido" => $task->task->origem_pedido,
+            "quem_pediu" => $task->task->quem_pediu,
+            "tipo_pedido" => $task->task->tipo_pedido
+        ];
+
+        $result_encoded = json_encode($json);
+
+        $curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'http://phc.brvr.pt:443/api/task',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'POST',
+		  CURLOPT_POSTFIELDS => $result_encoded,
+		  CURLOPT_HTTPHEADER => array(
+			'Content-Type: application/json'
+		  ),
+		));
+	
+	    curl_exec($curl);
+
+         
         Mail::to($task->task->tech->email)->queue(new TaskDispatchedTech($task));
 
         if($task->task->taskCustomer->email == null || !isset($task->task->taskCustomer->email))
@@ -44,6 +81,12 @@ class SendDispatchTasksNotification
         else {
             Mail::to($task->task->taskCustomer->email)->queue(new TaskDispatched($task));
         }
+
+        //envio do json para api
+        
+       
+
+        
         
     }
 }
--- app/Listeners/Tasks/SendTaskReportNotification.php
+++ app/Listeners/Tasks/SendTaskReportNotification.php
@@ -29,6 +29,67 @@ class SendTaskReportNotification
     public function handle(DispatchTaskReport $tasksReports)
     {
         $tasksReports = $tasksReports->tasksReports;
+
+    
+        foreach($tasksReports->servicesToDo as $i => $services)
+        {
+            $servicesArray[$i] = [
+                "descricao" => $services->additional_description,
+                "nome_servico" => $services->service->name
+            ];
+        }
+
+        foreach($tasksReports->getHoursTask as $i => $times)
+        {
+            $servicesTimes[$i] = [
+                "data_inicio" => $times->date_begin,
+                "hora_inicio" => $times->hour_begin,
+                "hora_final" => $times->hour_end,
+                "horas_total" => $times->total_hours,
+                "data_tempo_criado" => $times->created_at,
+                "descricao" => $times->descricao
+            ];
+        }
+
+
+        $json = [
+            "reference" => $tasksReports->reference, 
+            "customer_name" => $tasksReports->taskCustomer->name, 
+            "customer_nif" => $tasksReports->taskCustomer->vat,
+            "descricao" => $tasksReports->additional_description,
+            "data_prevista" => $tasksReports->scheduled_date,
+            "hora_prevista" => $tasksReports->scheduled_hour,
+            "data_criacao" => $tasksReports->created_at,
+            "nome_tecnico" => $tasksReports->tech->name,
+            "email_tecnico" => $tasksReports->tech->email,
+            "origem_pedido" => $tasksReports->tasks->origem_pedido,
+            "quem_pediu" => $tasksReports->tasks->quem_pediu,
+            "tipo_pedido" => $tasksReports->tasks->tipo_pedido,
+            "services" => $servicesArray,
+            "tempos" => $servicesTimes
+        ];
+
+         $result_encoded = json_encode($json);
+
+
+         $curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'http://phc.brvr.pt:443/api/times',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'POST',
+		  CURLOPT_POSTFIELDS => $result_encoded,
+		  CURLOPT_HTTPHEADER => array(
+			'Content-Type: application/json'
+		  ),
+		));
+	
+	    curl_exec($curl);
+
         Mail::to($tasksReports->tech->email)->queue(new TaskReportFinished($tasksReports));
         if($tasksReports->taskCustomer->email == null || !isset($tasksReports->taskCustomer->email))
         {
@@ -37,6 +98,9 @@ class SendTaskReportNotification
         else {
             Mail::to($tasksReports->taskCustomer->email)->queue(new TaskReportFinished($tasksReports));
         }
-        //Mail::to($tasksReports->taskCustomer->email)->queue(new TaskReportFinished($tasksReports));
+        
+        //enviar para o phc
+       
+
     }
 }
