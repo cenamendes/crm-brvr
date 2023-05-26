d2b61a469fd349fe0d56eb0c0581316df5b466f9
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-26 12:17:49 +0100

    [upd] ChatUsers

--- app/Http/Livewire/Tenant/ChatUsers/ChatUsers.php
+++ app/Http/Livewire/Tenant/ChatUsers/ChatUsers.php
@@ -55,7 +55,7 @@ class ChatUsers extends Component
 
     public function messagesRightSide($tech)
     {
-        $this->dispatchBrowserEvent("cleanChatBox"); 
+        // $this->dispatchBrowserEvent("cleanChatBox"); 
         $this->tech = $tech;
         $this->chat = $this->chatRepository->getMessagesBetweenTech($this->tech);
         $this->dispatchBrowserEvent("refreshChatPosition"); 
