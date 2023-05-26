63edcdde807c4944aa6d6a2eb003e54b7cadb21a
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-26 10:42:21 +0100

    [upd] task-dispatched

--- resources/views/tenant/mail/tasks/task-dispatched.blade.php
+++ resources/views/tenant/mail/tasks/task-dispatched.blade.php
@@ -98,6 +98,9 @@
                                                     <p style="font-family: Avenir, Helvetica, sans-serif; color: #74787e; ">
                                                         <b>{{__("Technical")}}: </b> {{$task->tech->name}}
                                                     </p>
+                                                    <p style="font-family: Avenir, Helvetica, sans-serif; color: #74787e;">
+                                                        <b>{{__("Who asked")}}: </b> {{$task->quem_pediu}}
+                                                    </p>
                                             </div>
                                             <hr>
                                                 <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
