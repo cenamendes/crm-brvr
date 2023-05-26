39d3ce8f06da5c32fe265a0a9358eee2d2ccc344
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-05-26 10:34:05 +0100

    [upd] ChatInterface

--- app/Interfaces/Tenant/ChatMessage/ChatInterface.php
+++ app/Interfaces/Tenant/ChatMessage/ChatInterface.php
@@ -5,12 +5,17 @@ namespace App\Interfaces\Tenant\ChatMessage;
 use App\Models\Tenant\Files;
 use App\Models\Tenant\ChatMessage;
 use Illuminate\Support\Collection;
+use App\Models\Tenant\ChatMessageTechnical;
 
 interface ChatInterface
 {
     public function getMessages($id): Collection;
 
     public function messageSend($message,$user_id,$customer_id): ChatMessage;
+
+    public function getMessagesBetweenTech($tech): Collection;
+
+    public function messageSendBetweenTech($message,$user_id,$tech_id): ChatMessageTechnical;
    
 
 }
