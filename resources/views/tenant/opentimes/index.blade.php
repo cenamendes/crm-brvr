81184ac338d9097c5c769d101d1dd6a376e35c64
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-06-12 16:57:07 +0100

    [upd] index open

---
+++ resources/views/tenant/opentimes/index.blade.php
@@ -0,0 +1,16 @@
+<x-tenant-layout title="{{ __('Open Times') }}" :themeAction="$themeAction">
+    <div class="container-fluid">
+        <div class="row">
+        
+                <!-- Add Order -->
+              
+                
+                <!-- row -->
+                
+                @livewire('tenant.open-times.show')
+
+               
+        </div>
+    </div>
+</x-tenant-layout>
+
