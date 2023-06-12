57d96b840d0110d6dc140f412e7ad579bd3b2149
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-06-12 16:46:16 +0100

    [new] Tasks Listeners

---
+++ app/Listeners/Tasks/TaskCustomerNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Listeners\Tasks;
+
+use App\Models\Tenant\Tasks;
+use App\Models\Tenant\Customers;
+use App\Events\Tasks\TaskCreated;
+use App\Models\Tenant\TeamMember;
+use App\Events\Tasks\TaskCustomer;
+use App\Mail\Tasks\TaskDispatched;
+use App\Mail\Tasks\TaskCreateEmail;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Tasks\TaskDispatchedTech;
+use App\Mail\Tasks\TaskReceiveEmailUser;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Events\Tasks\DispatchTasksToUser;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\Tasks\TasksDispatchedNotification;
+
+class TaskCustomerNotification
+{
+    use Notifiable;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\TaskCreated  $event
+     * @return void
+     */
+    public function handle(TaskCustomer $task)
+    {     
+        $customerEmail = Customers::where('id',$task->taskCustomer["customer_id"])->first();
+
+        Mail::to($customerEmail->email)->queue(new TaskReceiveEmailUser($task));
+
+    }
+}
---
+++ app/Listeners/Tasks/TaskNotification.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners\Tasks;
+
+use App\Models\Tenant\Tasks;
+use App\Events\Tasks\TaskCreated;
+use App\Models\Tenant\TeamMember;
+use App\Mail\Tasks\TaskDispatched;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Tasks\TaskDispatchedTech;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Events\Tasks\DispatchTasksToUser;
+use App\Mail\Tasks\TaskCreateEmail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\Tasks\TasksDispatchedNotification;
+
+class TaskNotification
+{
+    use Notifiable;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\TaskCreated  $event
+     * @return void
+     */
+    public function handle(TaskCreated $task)
+    {
+        $teamMember = TeamMember::where('id',$task->taskCreated->tech_id)->first();        
+        Mail::to($teamMember->email)->queue(new TaskCreateEmail($task));
+
+        
+    }
+}
