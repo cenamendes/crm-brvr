a005c8651f690f2c2605dbb9b04528bd21edeac6
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-06-12 16:47:16 +0100

    [upd] Task mails

---
+++ app/Mail/Tasks/TaskCreateEmail.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Mail\Tasks;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TaskCreateEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $task;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($task)
+    {
+        $this->task = $task->taskCreated;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+          //env('MAIL_USERNAME')
+        $subject = 'Tarefa #' . $this->task->reference . ' criada com sucesso.';
+        return new Envelope(
+            subject: $subject,
+            from: new Address(env('MAIL_USERNAME'), session('sender_name')),
+        );
+    }
+
+  
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    // public function content()
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    // public function attachments()
+    // {
+    //     return [];
+    // }
+
+    public function build()
+    {
+        $subject = 'Tarefa #' . $this->task->reference . ' criada com sucesso.';
+
+        $email = $this
+            ->view('tenant.mail.tasks.task-created',[
+                "subject" => $subject,
+                "task" => $this->task,
+                "company_name" => session('company_name'),
+                "vat" => session('vat'),
+                "contact" => session('contact'),
+                "email" => session('email'),
+                "address" => session('address'),
+                "logotipo" => session('logotipo'),
+            ]);
+
+            foreach($this->task->servicesToDo as $serv)
+            {
+                if($serv->service->file != null)
+                {
+                    $arrayFile = json_decode($serv->service->file,true);
+                
+                    foreach($arrayFile as $file)
+                    {
+                        $email->attach(config('filesystems.disks.local.root').$file["fileFolder"]);
+                    }
+                   
+                }
+            }
+
+            
+
+        return $email;
+    }
+
+}
