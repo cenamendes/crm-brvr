4f0179824cf218c29836dfc79a7eb4867acc4ee3
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-07-18 15:57:01 +0100

    [upd] aut login

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,7 +2,7 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <div class="font-medium text-red-600"><font color="white">
             {{ __('Whoops! Something went wrong.') }}
         </div>
 
