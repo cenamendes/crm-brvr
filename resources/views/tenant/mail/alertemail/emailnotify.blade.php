db7ff365c84badd84e2241eab6a97dc24defb20a
Joao Mendes <62259008+cenamendes@users.noreply.github.com>
2023-08-01 10:59:26 +0100

    [upd] emailnotify

--- resources/views/tenant/mail/alertemail/emailnotify.blade.php
+++ resources/views/tenant/mail/alertemail/emailnotify.blade.php
@@ -54,7 +54,7 @@
                                             </h1>
                                             <hr>
                                                 <p>Olá {{$customer->name}},</p><br>
-                                                <p>No dia {{$taskInfo["task"]["scheduled_date"]}} pelas {{$taskInfo["task"]["scheduled_hour"]}} será feita uma intervenção!</p>
+                                                <p>Relembrar que no dia {{$taskInfo["task"]["scheduled_date"]}} pelas {{$taskInfo["task"]["scheduled_hour"]}} será feita uma intervenção!</p>
                                             <hr>
                                                 <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
                                                     {{__("Compliments")}},<br>
